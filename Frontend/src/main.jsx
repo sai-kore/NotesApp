5c14d0d71a79bf1296b3bdc3712e336d66b37814
sai-kore <saikore550@gmail.com>
2026-06-06 18:12:59 +0530

    Initial commit

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,39 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import './index.css'
+
+// const promise = axios.get("http://localhost:3001/notes");
+// console.log(promise);
+// promise.then(response => {
+//   console.log(response);
+// })
+
+// axios.get("http://localhost:3001/notes").then((response) => {
+//   const notes = response.data;
+//   console.log(notes);
+// });
+
+// const promise2 = axios.get("http://localhost:5173/");
+// console.log(promise2);
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App notes={notes} />,
+);
