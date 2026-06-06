3acf2188419db7d527baa5753019e9f9ecf1ffb1
sai-kore <saikore550@gmail.com>
2026-06-06 20:23:21 +0530

    Changed Footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
     <div style={footerStyle}>
       <br />
       <p>
-        {/* Note app, Department of Computer Science, University of Helsinki 2025 */}
         Notes App, Made and Deployed by Sai Kore
       </p>
     </div>
